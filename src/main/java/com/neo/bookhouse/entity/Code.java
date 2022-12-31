5828845e22dfdeccc0c2af85fbdec7d77d201fed
hzf <2388263928@qq.com>
2023-01-01 00:03:13 +0800

    code change

--- src/main/java/com/neo/bookhouse/entity/Code.java
+++ src/main/java/com/neo/bookhouse/entity/Code.java
@@ -17,6 +17,62 @@ public class Code extends Model<Code>{
 	
 	private Date codeStart;
 	
+	public Long getCodeId() {
+		return codeId;
+	}
+
+	public void setCodeId(Long codeId) {
+		this.codeId = codeId;
+	}
+
+	public Long getBookId() {
+		return bookId;
+	}
+
+	public void setBookId(Long bookId) {
+		this.bookId = bookId;
+	}
+
+	public Short getCodeType() {
+		return codeType;
+	}
+
+	public void setCodeType(Short codeType) {
+		this.codeType = codeType;
+	}
+
+	public String getCodeContent() {
+		return codeContent;
+	}
+
+	public void setCodeContent(String codeContent) {
+		this.codeContent = codeContent;
+	}
+
+	public Date getCodeStart() {
+		return codeStart;
+	}
+
+	public void setCodeStart(Date codeStart) {
+		this.codeStart = codeStart;
+	}
+
+	public Date getCodeEnd() {
+		return codeEnd;
+	}
+
+	public void setCodeEnd(Date codeEnd) {
+		this.codeEnd = codeEnd;
+	}
+
+	public Short getCodeState() {
+		return codeState;
+	}
+
+	public void setCodeState(Short codeState) {
+		this.codeState = codeState;
+	}
+
 	private Date codeEnd;
 	
 	private Short codeState;
