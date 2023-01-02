11d1e5428728775d63f59eaab7e8fe8378f3a8df
西山对寂寥 <1781287641@qq.com>
2023-01-02 11:23:40 +0800

    更新 entity

--- src/main/java/com/neo/bookhouse/controller/UserController.java
+++ src/main/java/com/neo/bookhouse/controller/UserController.java
@@ -27,52 +27,28 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @GetMapping("/{id}")//用户信息：检查账号是否存在
-    public R<String> checkUserId(@PathVariable String id) {
-        log.info("id={}", id);
+    @GetMapping("/check/{userName}")
+    public R<String> checkUserName(@PathVariable String userName) {
         LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
-        queryWrapper.eq(User::getUserId, id);
-        User one = userService.getOne(queryWrapper);
-        if (one != null) {
-            return R.error("");
+        queryWrapper.eq(User::getUserName, userName);
+        User user = userService.getOne(queryWrapper);
+        if (user != null) {
+            return R.error("用户名已存在");
         }
-        return R.success("");
+        return R.success("用户名可用");
     }
 
-    @PostMapping
+    @PostMapping("/login")
     public R<User> login(HttpServletRequest request, @RequestBody User user) {
-        //1.将页面提交的密码进行md5加密处理
-        String password = user.getUserPassword();
-        password = md5(password);
-        //2.根据页面提交的用户名username查询数据库
-        LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
-        queryWrapper.eq(User::getUserId, user.getUserId());
-        User emp = userService.getOne(queryWrapper);
-        //3.如果没有查询到则返回登录失败结果
-        if (emp == null) {
-            return R.error("账号不存在");
-        }
-        //4.密码比对，如果不一致则返回登陆失败结果
-        if (!emp.getUserPassword().equals(password)) {
-            return R.error("密码错误");
-        }
-        //5.登录成功，将id存入session并返回登录成功结果
-        request.getSession().setAttribute("user", emp.getUserId());
-        emp.setUserPassword(null);
-        return R.success(emp);
+        log.info("请求登录：{}", user);
+
+
+        return R.error("登录失败");
     }
 
 
     @PostMapping("/register")//用户信息：注册账号信息
     public R<String> register(@RequestBody User user) {
-        LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
-        queryWrapper.eq(User::getUserId, user.getUserId());
-        User one = userService.getOne(queryWrapper);
-        if (one != null) {
-            return R.error("账号已存在");
-        }
-        String pwd = user.getUserPassword().trim();
-        user.setUserPassword(md5(pwd));
         boolean flg = userService.save(user);
         if (flg) {
             return R.success("注册成功");
@@ -83,11 +59,11 @@ public class UserController {
 
     @PutMapping
     public R<String> update(@RequestBody User user) {
-        //boolean flg = userService.updateById(user);
-        //if (flg) {
-        return R.success("修改成功");
-        //}
-        //return R.error("修改失败");
+        boolean flg = userService.updateById(user);
+        if (flg) {
+            return R.success("修改成功");
+        }
+        return R.error("修改失败");
     }
 
 
