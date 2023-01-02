a88ae76c6c234733a964303436dda3795af7f928
西山对寂寥 <1781287641@qq.com>
2023-01-02 14:08:17 +0800

    新增文件上下传功能

---
+++ src/main/java/com/neo/bookhouse/controller/FileController.java
@@ -0,0 +1,82 @@
+/**
+ * @Author: Neo
+ * @Date: 2023/01/02 星期一 13:52:10
+ * @Project: book_house
+ * @IDE: IntelliJ IDEA
+ **/
+package com.neo.bookhouse.controller;
+
+
+import com.neo.bookhouse.common.R;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.UUID;
+
+@Slf4j
+@RestController
+@RequestMapping("/file")
+public class FileController {
+
+    @Value("${bookhouse.imgPath}")
+    private String imgPath;
+
+    @PostMapping("/upload")
+    public R<String> upload(MultipartFile file) {
+        //file是一个临时文件，需要转存到指定位置，否则本次请求完成后临时文件会删除
+        log.info("file:{}", file.getName());
+        //原始文件名
+        String originalFilename = file.getOriginalFilename();
+        String suffix = originalFilename.substring(originalFilename.lastIndexOf("."));
+        //使用UUID重新生成文件名，防止文件名称重复造成文件覆盖
+        String fileName = UUID.randomUUID().toString() + suffix;
+        String path = System.getProperty("user.dir") + imgPath;
+        //创建一个目录对象
+        File dir = new File(path);
+        //判断当前目录是否存在
+        if (!dir.exists()) {
+            //如果不存在，则创建目录
+            dir.mkdirs();
+        }
+        try {
+            //将临时文件转存到指定位置
+            file.transferTo(new File(path + fileName));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return R.success(fileName);
+    }
+
+    @GetMapping("/download/{fileName}")
+    public void download(@PathVariable String fileName, HttpServletResponse response) {
+        try {
+            //输入流，通过输入流读取文件内容
+            String path = System.getProperty("user.dir") + imgPath;
+            FileInputStream fileInputStream = new FileInputStream(new File(path + fileName));
+            //输出流，通过输出流将文件写回浏览器，在浏览器展示图片
+            ServletOutputStream outputStream = response.getOutputStream();
+
+            response.setContentType("image/jpeg");
+
+            byte[] bytes = new byte[1024];
+            int len = 0;
+
+            while ((len = fileInputStream.read(bytes)) != -1) {
+                outputStream.write(bytes, 0, len);
+                outputStream.flush();
+            }
+            //关闭资源
+            outputStream.close();
+            fileInputStream.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
